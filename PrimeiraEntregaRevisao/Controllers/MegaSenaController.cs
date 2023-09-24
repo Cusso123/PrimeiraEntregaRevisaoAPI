c3868a7ab3b272ba953c9d96c3c6e18bb102ba8a
MarcusCosta <marcu@DESKTOP-LPOVEDR>
2023-09-24 18:49:15 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiraEntregaRevisao/Controllers/MegaSenaController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PrimeiraEntregaRevisao.Model;
+
+namespace PrimeiraEntregaRevisao.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MegaSenaController : ControllerBase
+    {
+
+        private readonly string _jogoCaminhoArquivo;
+
+        public MegaSenaController()
+        {
+            _jogoCaminhoArquivo = Path.Combine(Directory.GetCurrentDirectory(), "FileJsonNumero", "Numeros.json");
+        }
+
+        #region Métodos arquivo
+        private List<RegistroJogo> LerJogosDoArquivo()
+        {
+            if (!System.IO.File.Exists(_jogoCaminhoArquivo))
+            {
+                return new List<RegistroJogo>();
+            }
+
+            string json = System.IO.File.ReadAllText(_jogoCaminhoArquivo);
+            return JsonConvert.DeserializeObject<List<RegistroJogo>>(json);
+        }       
+
+        private void EscreverJogoNoArquivo(List<RegistroJogo> produtos)
+        {
+            string json = JsonConvert.SerializeObject(produtos);
+            System.IO.File.WriteAllText(_jogoCaminhoArquivo, json);
+        }
+        #endregion
+
+
+
+        [HttpPost]
+        public IActionResult Post(RegistroJogo registroJogo)
+        {
+            if(registroJogo.PrimeiroNum != registroJogo.SegundoNum
+              && registroJogo.SegundoNum != registroJogo.TerceiroNum
+              && registroJogo.TerceiroNum != registroJogo.QuartoNum
+              && registroJogo.QuartoNum != registroJogo.QuintoNum
+              && registroJogo.QuintoNum != registroJogo.SextoNum
+
+              )
+            {
+                var listaJogos = LerJogosDoArquivo();
+                listaJogos.Add(registroJogo);
+                EscreverJogoNoArquivo(listaJogos);
+                return Ok("Jogo Registrado com sucesso!");
+
+            }
+            else
+            {
+                return BadRequest("Existem numeros repetidos, o jogo não pode ser gravado!");
+
+            }
+
+        }
+
+            [HttpGet] public IActionResult Get()
+            {
+                return Ok(LerJogosDoArquivo());
+            }
+    }
+}
---
+++ PrimeiraEntregaRevisao/ViewModels/RegistroJogo.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrimeiraEntregaRevisao.Model
+{
+    public class RegistroJogo
+    {
+        [MinLength(3, ErrorMessage = "Minimo de 3 letras.")]
+        [MaxLength(255, ErrorMessage = "maximo ....")]
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        public string CPF { get; set; }
+
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        [Range(1, 60)]
+        public int PrimeiroNum { get; set; }
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        [Range(1, 60)]
+        public int SegundoNum { get; set; }
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        [Range(1, 60)]
+        public int TerceiroNum { get; set; }
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        [Range(1, 60)]
+        public int QuartoNum { get; set; }
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        [Range(1, 60)]
+        public int QuintoNum { get; set; }
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        [Range(1, 60)]
+        public int SextoNum { get; set; }   
+        public DateTime DataDoJogo { get; set; }
+    }
+}
